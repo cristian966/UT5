b51a6d076cc79e633c825d6ed73c21cb24d841b5
cristian <cperezavila@infomm.local>
2020-01-27 20:05:50 +0100

    rama master

---
+++ build/precache-manifest.9d51f3fe16a40cdfd8c9e5a0dfc8d748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a579e64cee5a8ad29d980ee205bca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375e2a1a8cd3046e69e7",
+    "url": "/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "f41f09a282cfcd6fab21",
+    "url": "/static/js/2.7da0e140.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7da0e140.chunk.js.LICENSE"
+  },
+  {
+    "revision": "375e2a1a8cd3046e69e7",
+    "url": "/static/js/main.454cc1cc.chunk.js"
+  },
+  {
+    "revision": "80d75ca03e2a131dde5e",
+    "url": "/static/js/runtime-main.fe966dac.js"
+  }
+]);
